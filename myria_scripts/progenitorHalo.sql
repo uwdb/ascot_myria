7985dd9349720c73b7b8cc0c61edd9c74aaff9a9
jortiz16 <jortiz16@cs.washington.edu>
2014-12-12 18:09:39 -0800

    prog edits

--- myria_scripts/progenitorHalo.sql
+++ myria_scripts/progenitorHalo.sql
@@ -2,36 +2,38 @@
 edges = scan(public:vulcan:edgesTree);
 haloTable = scan(public:vulcan:haloTable);
 
-progentors = [from edges where currentTime = 1 emit nowGroup, currentTime, currentGroup];
+progenitors = [from edges where currentTime = 1 emit nowGroup, int(currentTime) as currentTime, currentGroup, nextGroup, sharedParticleCount];
 
 I = [2 as i];
-do 
-maxShared = [from edges emit nowGroup,currentTime,currentGroup, max(SharedParticleCount) as maxSharedParticleCount];
-getMaxInfo = [from edges as e, maxShared as m where e.currentTime+1 = I.i and e.nowGroup = m.nowGroup and e.currentTime = m.currentTime and e.currentGroup = m.currentGroup and e.SharedParticleCount = m.maxSharedParticleCount emit e.currentTime, e.currentGroup, e.nextGroup, e.sharedParticleCount];
-delta = DISTINCT([from getMaxInfo as g emit g.NowGroup, g.currentGroup, g.currentTime+1 as currentTime, g.nextGroup, g.sharedParticleCount]);
-I = [from I emit i+1];
-progenitors = progenitors + delta;
-while [from I where i < 10 emit i]
 
-store(progenitors, public:vulcan:prog);
+do
+maxShared = [from edges emit nowGroup,currentTime,currentGroup, max(sharedParticleCount) as maxSharedParticleCount];
 
---join with haloTable
---haloTable: nowGroup, grpID, timestep...
+getMaxInfo = [from edges as e, maxShared as m, I where int(e.currentTime+1) = int(I.i) and e.nowGroup = m.nowGroup and e.currentTime = m.currentTime and e.currentGroup = m.currentGroup and e.sharedParticleCount = m.maxSharedParticleCount emit e.nowGroup, e.currentTime, e.currentGroup, e.nextGroup, e.sharedParticleCount];
 
-findProg = [from haloTable h, progenitors p where h.nowGroup = p.nowGroup and h.grpID = p.currentGroup and h.currentTime = p.timestep emit h.*];
+delta = DISTINCT([from getMaxInfo as g emit g.nowGroup, int(g.currentTime+1) as currentTime,  g.currentGroup, g.nextGroup, g.sharedParticleCount]);
+
+I = [from I emit i+1 as i];
+
+progenitors = distinct(progenitors + delta);
+
+while [from I emit min(i) < 7];
+
+progenitors = [from progenitors emit nowGroup, int(currentTime) as currentTime, currentGroup, nextGroup, sharedParticleCount];
+
+store(progenitors, public:vulcan:progen);
+
+findProg = [from haloTable h, progenitors p where h.nowGroup = p.nowGroup and h.grpID = p.currentGroup and h.timeStep = int(p.currentTime) emit h.*];
+
 labelProg = [from findProg as f emit f.*, 1 as prog];
 
-findNonProg = haloTable - findProg;
+findNonProg = diff(haloTable,findProg);
 labelNonProg = [from findNonProg as f emit f.*, 0 as prog];
 
-haloTableNew = labelProg + labelNonProg;
+haloTableNew = distinct(labelProg + labelNonProg);
 
 final = [from haloTableNew emit *, -1 as massRatio];
 
 store(final, public:vulcan:haloTableProg);
-
-
-
-
 
 
