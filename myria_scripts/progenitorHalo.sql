0281a3b575a9a9b09ad3ba9b095e978ee383cd24
jortiz16 <jortiz16@cs.washington.edu>
2014-12-15 22:53:27 -0800

    progenitor modify

--- myria_scripts/progenitorHalo.sql
+++ myria_scripts/progenitorHalo.sql
@@ -1,25 +1,34 @@
 --nowGroup, currentTime, currentGroup, nextGroup, sharedParticleCount
-edges = scan(public:vulcan:edgesTree);
-haloTable = scan(public:vulcan:haloTable);
+edges = scan(public:adhoc:treeEdges);
 
-progenitors = [from edges where currentTime = 1 emit nowGroup, int(currentTime) as currentTime, currentGroup, nextGroup, sharedParticleCount];
+--the first progenitor
+progenitors = [from edges where currentTime = 1 emit nowGroup, int(currentTime) as currentTime, currentGroup];
+
+maxShared = [from edges emit nowGroup,currentTime,currentGroup, max(sharedParticleCount) as maxSharedParticleCount];
 
 I = [2 as i];
 
+--loop
 do
-maxShared = [from edges emit nowGroup,currentTime,currentGroup, max(sharedParticleCount) as maxSharedParticleCount];
+--edges linked to previous prog
+prevProg = distinct([from progenitors, I where currentTime+1 = I.i emit nowGroup, currentTime, currentGroup]);
 
-getMaxInfo = [from edges as e, maxShared as m, I where int(e.currentTime+1) = int(I.i) and e.nowGroup = m.nowGroup and e.currentTime = m.currentTime and e.currentGroup = m.currentGroup and e.sharedParticleCount = m.maxSharedParticleCount emit e.nowGroup, e.currentTime, e.currentGroup, e.nextGroup, e.sharedParticleCount];
+--get the groups and emit the maxShared
+nextGroups = [from prevProg p, edges e where e.nowGroup = p.nowGroup and int(e.currentTime) = p.currentTime and e.currentGroup = p.currentGroup emit max(e.sharedParticleCount) as maxShared];
 
-delta = DISTINCT([from getMaxInfo as g emit g.nowGroup, int(g.currentTime+1) as currentTime,  g.currentGroup, g.nextGroup, g.sharedParticleCount]);
+--get the group
+maxGroup = distinct([from nextGroups g, edges e, I where e.currentTime+1 = I.i and e.sharedParticleCount = g.maxShared emit e.nowGroup, (e.currentTime+1) as currentTime, e.nextGroup as currentGroup]);
+
+--union
+progenitors = distinct(progenitors + maxGroup);
 
 I = [from I emit i+1 as i];
-
-progenitors = distinct(progenitors + delta);
 
 while [from I emit min(i) < 7];
 
 progenitors = [from progenitors emit nowGroup, int(currentTime) as currentTime, currentGroup, nextGroup, sharedParticleCount];
+
+--tested up to here
 
 store(progenitors, public:vulcan:progen);
 
