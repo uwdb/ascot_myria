7f37f82f7aa4f0b1b64118024076f60c0eaa1af2
jortiz16 <jortiz16@cs.washington.edu>
2014-12-17 19:37:25 -0800

    script fixed link

--- server.js
+++ server.js
@@ -188,6 +188,6 @@ app.get('/myria/getdata', function(req, postResponse){
 });
 
 if (!module.parent) {
-  app.listen(8016);
+  app.listen(8017);
   console.log("ASCOT server listening on port %d", app.address().port);
 }
