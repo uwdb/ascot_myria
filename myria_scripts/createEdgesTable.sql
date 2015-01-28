169d6f90b0fdfc87a84bb97d6e21e72d20ef64b2
jortiz16 <jortiz16@cs.washington.edu>
2015-01-28 15:21:38 -0800

    minor

--- myria_scripts/createEdgesTable.sql
+++ myria_scripts/createEdgesTable.sql
@@ -2,7 +2,7 @@ haloTable = scan(public:vulcan:haloTable);
 particleTable = scan(public:vulcan:particlesOfInterest);
 nowGroups = [from haloTable ht, particleTable pt where ht.nowGroup = pt.nowGroup and ht.grpID = pt.grp and ht.timeStep = pt.timestep and ht.timeStep = 1 emit ht.nowGroup, ht.grpID, ht.timeStep, ht.mass, pt.iOrder];
 edgesInit = [from particleTable pt1, particleTable pt2, haloTable ht1, haloTable ht2, nowGroups ng
-        where pt1.grp = ht1.grpID and pt1.timestep = ht1.timeStep and pt1.iOrder = pt2.iOrder and pt1.iOrder = ng.iOrder and pt2.nowGroup = ht2.nowGroup and pt2.grp = ht2.grpID and pt2.timestep = ht2.timeStep and ht1.timeStep+1 = ht2.timeStep
+        where pt1.grp = ht1.grpID and pt1.timestep = ht1.timeStep and pt1.iOrder = pt2.iOrder and pt1.iOrder = int(ng.iOrder) and pt2.nowGroup = ht2.nowGroup and pt2.grp = ht2.grpID and pt2.timestep = ht2.timeStep and ht1.timeStep+1 = ht2.timeStep
         emit ng.nowGroup, ht1.timeStep as currentTime, ht1.grpID as currentGroup, ht2.grpID as nextGroup, count(*) as sharedParticleCount];
 -- filter edges on number of particles shared
 edges = [from edgesInit where sharedParticleCount > 256 emit *];
